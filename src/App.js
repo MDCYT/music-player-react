2feb24039284d304a5979efe510a292621fabe4b
MDCDEV <me@mdcdev.me>
2022-11-24 00:33:33 -0500

    Update, i think

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 import './App.css';
+// Add a function for the play or pause button
+import './js/player.js';
 
-function App() {
+function App() {  
+
   return (
     <div className="App">
+      <audio id="audio" style={{ display: 'none' }} preload="metadata" title='Ghosts - Jacob Tillberg'>
+        <source src="/songs/music.mp3" type="audio/mpeg" />
+      </audio>
       <header className="App-header bg-gradient-to-t from-[#261130] to-[#140D1D] h-screen">
         {/* Make a div square with border color and border radius */}
-        <div className="border-[#991367] border-[3px] w-96 h-96"></div>
-        { /* Make 3 columns for buttons of music player, the buttons are font awesome icons and is, list, like and share */}
+        {/* <div className="border-[#991367] border-[3px] w-96 h-96 bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: "url('/img/zim.png')" }}></div> */}
+        { /* Make the same but with a canvas */}
+        <canvas id="canvas" className="border-[#991367] border-[3px] w-96 h-96 bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: "url('/img/zim.png')" }}></canvas>
         <div className="flex flex-row justify-center w-96 my-4">
           <div className="flex flex-row gap-32">
-            <i class="fas fa-list text-[#991367] text-4xl"></i>
-            <i class="fa-regular fa-heart text-[#991367] text-4xl"></i>
-            <i class="fas fa-share text-[#991367] text-4xl"></i>
+            <i className="fas fa-list text-[#991367] text-4xl"></i>
+            <i className="fa-regular fa-heart text-[#991367] text-4xl"></i>
+            <i className="fas fa-share text-[#991367] text-4xl"></i>
           </div>
         </div>
         {/* Make a div for a song title */}
         <div className="flex flex-row justify-center w-96">
-          <div className="text-[#991367] text-2xl">The Rebellion</div>
+          <div className="text-[#991367] text-2xl" id="titleSong">The Rebellion</div>
         </div>
         {/* Make a div for a song artist */}
         <div className="flex flex-row justify-center w-96">
-          <div className="text-[#991367] text-xl">Invaders Must Die</div>
+          <div className="text-[#991367] text-xl" id="authorSong">Invaders Must Die</div>
         </div>
         {/* Make a div for the progress bar, the progress bar is a range input, the rage input have a rect border and have the same color of the border of the div */}
         <div className="flex flex-row justify-center w-96 my-4">
-          <input type="range" className="w-96 border-[#991367] border-[3px] rounded-[10px] accent-[#991367]"></input>
+          <input type="range" className="w-96 border-[#991367] border-[3px] rounded-[10px] accent-[#991367]" id="progressBar" defaultValue={0} />
         </div>
         {/* Make a div for the time of the song */}
         <div className="flex flex-row justify-center w-96">
-          <div className="text-[#991367] text-xl">1:17</div>
+          <div className="text-[#991367] text-xl time-song">0:00 / 0:00</div>
         </div>
         {/* Make a div for the buttons of the music player, the buttons are font awesome icons and is, list, like and share */}
         <div className="flex flex-row justify-center w-96 my-4">
           <div className="flex flex-row gap-32">
-            <i class="fas fa-random text-[#991367] text-4xl"></i>
-            <i class="fas fa-step-backward text-[#991367] text-4xl"></i>
-            <i class="fas fa-play text-[#991367] text-4xl"></i>
-            <i class="fas fa-step-forward text-[#991367] text-4xl"></i>
-            <i class="fas fa-redo text-[#991367] text-4xl"></i>
+            <i className="fas fa-random text-[#991367] text-4xl"></i>
+            <i className="fas fa-step-backward text-[#991367] text-4xl"></i>
+            <i className="fas fa-play text-[#991367] text-4xl paused transition-all" id='play'></i>
+            <i className="fas fa-step-forward text-[#991367] text-4xl transition-all"></i>
+            <i className="fas fa-redo text-[#991367] text-4xl opacity-50" id='repeat'></i>
           </div>
         </div>
       </header>
---
+++ src/js/player.js
@@ -0,0 +1,173 @@
+window.addEventListener('DOMContentLoaded', async (event) => {
+    const play = document.getElementById('play');
+    const repeat = document.getElementById('repeat');
+    const audio = document.getElementById('audio');
+    const progressBar = document.getElementById('progressBar');
+    const title = document.getElementById('titleSong');
+    const author = document.getElementById('authorSong');
+    const img = new Image();
+    img.src = '/img/zim.png';
+    // wait for change the image
+    const time = document.querySelector('.time-song');
+
+    let updateTime = () => {};
+    let updateTitleAndArtist = () => {};
+    audio.onloadedmetadata = async function() {
+        updateTime = () => {
+            let min = Math.floor(audio.currentTime / 60)
+            let sec = Math.floor(audio.currentTime % 60)
+            let allTime = Math.floor(audio.duration);
+            let allMin = Math.floor(allTime / 60);
+            let allSec = Math.floor(allTime % 60);
+            if(sec < 10) {
+                sec = `0${sec}`;
+            }
+            time.innerText = `${min}:${sec} / ${allMin}:${allSec}`;
+            progressBar.value = (audio.currentTime / audio.duration) * 100;
+            //Chnage the max value of the progress bar
+            progressBar.maxLength = audio.duration;
+        }
+
+        updateTime();
+
+        updateTitleAndArtist = () => {
+            let titleAndArtist = audio.title.split(' - ');
+            title.innerText = titleAndArtist[0];
+            author.innerText = titleAndArtist[1];
+        }
+
+        updateTitleAndArtist();
+    }
+
+    play.addEventListener('click', () => {
+        if(play.classList.contains('paused')) {
+            play.classList.remove('paused');
+            play.classList.remove('fa-play');
+            play.classList.add('fa-pause');
+            audio.play();
+        } else {
+            play.classList.add('paused');
+            play.classList.add('fa-play');
+            play.classList.remove('fa-pause');
+            audio.pause();
+        }
+    });
+
+    play.addEventListener('mouseover', () => {
+        play.style.cursor = 'pointer';
+        play.style.transform = 'scale(1.1)';
+        play.classList.add('hover');
+    });
+
+    play.addEventListener('mouseout', () => {
+        play.style.cursor = 'default';
+        play.style.transform ='scale(1)';
+        play.classList.remove('hover')
+    });
+
+
+    audio.addEventListener('timeupdate', () => {
+        updateTime();
+    });
+
+    progressBar.addEventListener('change', () => {
+        audio.currentTime = progressBar.value;
+        renderFrame();
+        if(!repeat.classList.contains('active') && Math.floor(audio.currentTime) === Math.floor(audio.duration)) {
+            play.classList.add('paused');
+            play.classList.add('fa-play');
+            play.classList.remove('fa-pause');
+            audio.pause();
+        }
+    });
+
+    repeat.addEventListener('click', () => {
+        if(audio.loop) {
+            audio.loop = false;
+            repeat.classList.remove('opacity-100');
+            repeat.classList.add('opacity-50');
+            repeat.classList.remove('active');
+        } else {
+            audio.loop = true;
+            repeat.classList.add('opacity-100');
+            repeat.classList.remove('opacity-50');
+            repeat.classList.add('active');
+        }
+    });
+
+    repeat.addEventListener('mouseover', () => {
+        repeat.style.cursor = 'pointer';
+        repeat.style.transform = 'scale(1.1)';
+        repeat.classList.add('hover');
+    });
+
+    repeat.addEventListener('mouseout', () => {
+        repeat.style.cursor = 'default';
+        repeat.style.transform ='scale(1)';
+        repeat.classList.remove('hover')
+    });
+
+    const canvas = document.getElementById("canvas")
+    const ctx = canvas.getContext("2d")
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    //Make a music visualizer
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioCtx.createAnalyser();
+    const source = audioCtx.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    analyser.fftSize = 128;
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const WIDTH = canvas.width
+    const HEIGHT = canvas.height
+
+    const barWidth = (WIDTH / bufferLength) * 2;
+    let barHeight;
+    let x = 0;
+
+    ctx.imageSmoothingEnabled = false;
+
+    function renderFrame() {
+        requestAnimationFrame(renderFrame);
+
+        x = 0;
+
+        analyser.getByteFrequencyData(dataArray);
+
+        ctx.clearRect(0,0,WIDTH,HEIGHT);
+
+
+        for(let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] * 8;
+
+            // const r = barHeight + (25 * (i/bufferLength));
+            // const g = 250 * (i/bufferLength);
+            // const b = 50;
+
+            // Make a rgb, if is a big number, the color is magenta, if is a small number, the color is pink, the colors is in a degradado
+            const r = 255;
+            const g = 0;
+            const b = 255 - (255 * (i/bufferLength));
+
+            ctx.fillStyle = `rgb(${r}, ${g}, ${b}, 0.2)`;
+            ctx.fillRect(x, (HEIGHT - barHeight), barWidth, barHeight);
+
+            x += barWidth + 1;
+
+            // Put the image in the canvas
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+            
+        }
+
+    }
+
+    renderFrame();
+
+    
+});
